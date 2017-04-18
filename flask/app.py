69933fd6007748d5775014c5b2a047b7f0dec8a4
Kiran Karpurapu <kvk229@nyu.edu>
2017-04-18 02:12:55 -0400

    flask ready to upload to S3

--- flask/app.py
+++ flask/app.py
@@ -3,6 +3,11 @@ import base64
 import boto
 import sys
 import os
+import json
+import dlib
+import scipy.ndimage
+from scipy.ndimage import imread
+import numpy as np
 from boto.s3.key import Key
 
 app = Flask(__name__)
@@ -17,6 +22,7 @@ def write_to_file(filename, file_data):
 	file = open(filename, 'w')
 	file.write(file_data)
 	file.close()
+	return file
 
 
 def percent_cb(complete, total):
@@ -25,21 +31,47 @@ def percent_cb(complete, total):
 
 def upload_to_s3(filename):
 	key = Key(bucket)
-	key.key = image_name
+	key.key = filename
 	key.set_contents_from_filename(filename, cb = percent_cb, num_cb = 10)
+	url = key.generate_url(expires_in=300)
+	return url
+
+def get_face_encodings(path_to_image):
+	print("The path of the photo is ", path_to_image)
+	# path_to_image = '/Users/kirank/Documents/recongnito/images/kiran.jpg'
+	path_to_image ='/Users/kirank/Documents/recongnito/flask/kiran.jpg'
+	face_detector = dlib.get_frontal_face_detector()
+	shape_predictor = dlib.shape_predictor('./../shape_predictor_68_face_landmarks.dat')
+	face_recognition_model = dlib.face_recognition_model_v1('./../dlib_face_recognition_resnet_model_v1.dat')
+	TOLERANCE = 0.6
+	image = scipy.ndimage.imread(path_to_image)
+	detected_faces = face_detector(image, 1)
+	for face in detected_faces:
+		print("detected the following face, ", face)
+	shapes_faces = [shape_predictor(image, face) for face in detected_faces]
+	return [np.array(face_recognition_model.compute_face_descriptor(image, face_pose, 1)) for face_pose in shapes_faces]	
+
+def write_to_database(url, json, image):
+	encodings = get_face_encodings(image)
+	print("The encoding for the photo:", encodings)
+	return 1
 
 
-@app.route('/image', methods=['POST'])
+@app.route('/newImage', methods=['POST'])
 def image():
 	if request.method == 'POST':
 		data = request.form
 		base64_data = data['base64']
-		print(len(base64_data))
 		image_data = base64.b64decode(base64_data)
 		image_name = str(data['ImageName'])
-		write_to_file(image_name, image_data)
+		image_info = str(data['ImageInfo'])
+		image_json = json.loads(image_info)
+		file = write_to_file(image_name, image_data)
 		print("****************************")
-		upload_to_s3(image_name)
+		# url = upload_to_s3(image_name)
+		url = "kiran"
+		write_to_database(url, image_json, image_name)
+		print("Image info", image_json['name'], url)
 	return "Yo"   
 
 if __name__ == '__main__':
